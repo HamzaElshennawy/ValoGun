515ee7ee3a75bd6ed92f92fd4fd9edbd9f209f62
HamzaElshennawy <h.elshennawy@nu.edu.eg>
2023-10-26 17:18:09 +0300

    .

--- ValoGun/MauiProgram.cs
+++ ValoGun/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using ValoGun.ViewModels;
 using CommunityToolkit.Maui;
 using ValoGun.Pages;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 
 namespace ValoGun
@@ -10,12 +11,13 @@ namespace ValoGun
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            builder.UseMauiApp<App>().UseSkiaSharp().ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("BowlbyRegular.ttf", "BowlbyRegular");
             }).UseMauiCommunityToolkit();
+			
             builder.Services.AddSingleton<HomePageViewModel>();
             builder.Services.AddTransient<WeaponsViewModel>();
 			builder.Services.AddSingleton<HomePage>();
